72141d501e3608cec8c8ae020a2d1771ec6d3cc5
luis-fk <felipekorbes@live.com>
2025-01-22 18:47:09 -0300

    initial setup

--- plants/views.py
+++ plants/views.py
@@ -1,8 +1,11 @@
-from rest_framework.views import APIView
+from django.http import HttpRequest
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from . import serializers
 
+
 class HelloWorldView(APIView):
-    def get(self, request, *args, **kwargs):
-        serializer = serializers.HelloWorldSerializer({'message': 'Hello, World!'})
+    def get(self, request: HttpRequest, *args: list, **kwargs: dict) -> Response:
+        serializer = serializers.HelloWorldSerializer({"message": "Hello, World!"}) 
         return Response(serializer.data)
