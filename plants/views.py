48ab23da6c46d4526ed4f75573aeb16f2185cd0a
luis-fk <felipekorbes@live.com>
2025-01-22 19:26:53 -0300

    removed libs and formatted files

--- plants/views.py
+++ plants/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpRequest
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from . import serializers
+from views import serializers
 
 
 class HelloWorldView(APIView):
     def get(self, request: HttpRequest, *args: list, **kwargs: dict) -> Response:
-        serializer = serializers.HelloWorldSerializer({"message": "Hello, World!"}) 
+        serializer = serializers.HelloWorldSerializer({"message": "Hello, World!"})
         return Response(serializer.data)
