8ede01624f449a881afa24ca8432f5d0c7548b52
luis-fk <felipekorbes@live.com>
2025-03-30 18:41:28 -0300

    fix websearch tool call

--- plants/api/chatbot/tools.py
+++ plants/api/chatbot/tools.py
@@ -1,8 +1,10 @@
 from typing import Any
 
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.tools import tool
 
 
+@tool
 def web_search(web_search_input: str) -> Any:
     """Returns web search related questions."""
     search = TavilySearchResults(max_results=3)
