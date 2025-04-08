bc874f00f254c8fa05e6fcfebfb550012cb2e2f5
luis-fk <felipekorbes@live.com>
2025-04-08 19:50:18 -0300

    inital political culture files

--- political_culture/admin.py
+++ political_culture/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from political_culture.models import Texts, TextWordCount
+
+admin.site.register(Texts)
+admin.site.register(TextWordCount)
