bd9b52c759c68adc7ff4a617c122fc075426113f
luis-fk <fk.luis@outlook.com>
2026-05-11 11:04:24 -0300

    code improvements

--- cfflch/api/admission_status/consumers.py
+++ cfflch/api/admission_status/consumers.py
@@ -14,6 +14,9 @@ class AdmissionStatusConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code: int) -> None:
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
+    async def receive(self, text_data: str) -> None:
+        pass
+
     async def admission_status_message(self, event: dict) -> None:
         message = event["message"]
         await self.send(text_data=json.dumps(message))
