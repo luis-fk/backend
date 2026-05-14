b7b1e7b4e19e512c6d5ca32a7e890d39c12f0a42
luis-fk <fk.luis@outlook.com>
2026-05-14 17:49:18 -0300

    added more details to the views on the adming page

--- cfflch/admin.py
+++ cfflch/admin.py
@@ -1,1 +1,44 @@
-# Register your models here.
+from django.contrib import admin
+
+from cfflch.models import AdmissionPDF, AdmissionResult, ClassRoom, Users
+
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ["id", "auth_user_id"]
+    search_fields = ["auth_user_id"]
+
+
+@admin.register(ClassRoom)
+class ClassRoomAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "name_normalized"]
+    search_fields = ["name"]
+
+
+class AdmissionPDFInline(admin.TabularInline):
+    model = AdmissionPDF
+    extra = 0
+    fields = ["url", "search_title"]
+
+
+@admin.register(AdmissionResult)
+class AdmissionResultAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "student_name",
+        "year",
+        "approved",
+        "class_room",
+        "created_at",
+    ]
+    list_filter = ["year", "approved", "class_room"]
+    search_fields = ["student_name"]
+    date_hierarchy = "created_at"
+    inlines = [AdmissionPDFInline]
+
+
+@admin.register(AdmissionPDF)
+class AdmissionPDFAdmin(admin.ModelAdmin):
+    list_display = ["id", "result", "search_title", "url"]
+    search_fields = ["search_title", "url"]
+    raw_id_fields = ["result"]
--- political_culture/admin.py
+++ political_culture/admin.py
@@ -10,10 +10,49 @@ from political_culture.models import (
     Users,
 )
 
-admin.site.register(Texts)
-admin.site.register(TextWordCount)
-admin.site.register(UserMemory)
-admin.site.register(Users)
-admin.site.register(IdeologiesDefinition)
-admin.site.register(TextChunks)
-admin.site.register(ChatHistory)
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ["id", "auth_user_id"]
+    search_fields = ["auth_user_id"]
+
+
+@admin.register(Texts)
+class TextsAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "author", "ideology", "user_submitted_text", "user"]
+    list_filter = ["ideology", "user_submitted_text"]
+    search_fields = ["title", "author", "ideology"]
+    raw_id_fields = ["user"]
+
+
+@admin.register(TextWordCount)
+class TextWordCountAdmin(admin.ModelAdmin):
+    list_display = ["id", "text", "total_word_count"]
+    search_fields = ["text__title"]
+    raw_id_fields = ["text"]
+
+
+@admin.register(IdeologiesDefinition)
+class IdeologiesDefinitionAdmin(admin.ModelAdmin):
+    list_display = ["id", "ideology"]
+    search_fields = ["ideology"]
+
+
+@admin.register(TextChunks)
+class TextChunksAdmin(admin.ModelAdmin):
+    list_display = ["id", "text"]
+    raw_id_fields = ["text"]
+
+
+@admin.register(ChatHistory)
+class ChatHistoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "role", "message"]
+    list_filter = ["role"]
+    search_fields = ["message"]
+    raw_id_fields = ["user"]
+
+
+@admin.register(UserMemory)
+class UserMemoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "user"]
+    raw_id_fields = ["user"]
