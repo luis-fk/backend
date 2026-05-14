ce6c31e6fb35cbcf04ae4cb080453d1a46b4ee21
luis-fk <fk.luis@outlook.com>
2026-05-14 17:15:25 -0300

    added user auth to cfflch

--- cfflch/api/urls.py
+++ cfflch/api/urls.py
@@ -6,10 +6,11 @@ from cfflch.api.admission_results.routes import (
 )
 from cfflch.api.admission_status.routes import AdmissionStatusApi
 from cfflch.api.class_rooms.routes import ClassRoomsListApi
+from cfflch.api.users.routes import UserApi
 
 admission_status_urls = [
     path(
-        "api/cfflch/admission_status",
+        "api/cfflch/admission-status/",
         AdmissionStatusApi.as_view(),
         name="admission_status",
     ),
@@ -36,4 +37,14 @@ class_rooms_urls = [
     ),
 ]
 
-cfflch_urls = admission_status_urls + admission_results_urls + class_rooms_urls
+users_urls = [
+    path(
+        "api/cfflch/users/<str:name>",
+        UserApi.as_view(),
+        name="cfflch_user",
+    ),
+]
+
+cfflch_urls = (
+    admission_status_urls + admission_results_urls + class_rooms_urls + users_urls
+)
